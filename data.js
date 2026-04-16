cd3f05635788fe0c87cfb004d4443c1591071543
M0B4 <barz.moritz@t-online.de>
2026-04-16 20:00:33 +0200

    logos

--- data.js
+++ data.js
@@ -21,6 +21,30 @@ const projects = [{
         previewImage: "images/3d-preview.jpg",
         description: "Hochpräzise technische Bauteile für industrielle Anwendungen. Druck- und stressgetestet.",
         images: ["images/3d-1.jpg", "images/3d-2.jpg"]
+    },
+    {
+        id: "project-3d",
+        title: "Industrial 3D Parts",
+        category: "3d-print",
+        previewImage: "images/3d-preview.jpg",
+        description: "Hochpräzise technische Bauteile für industrielle Anwendungen. Druck- und stressgetestet.",
+        images: ["images/3d-1.jpg", "images/3d-2.jpg"]
+    },
+    {
+        id: "project-3d",
+        title: "Industrial 3D Parts",
+        category: "3d-print",
+        previewImage: "images/3d-preview.jpg",
+        description: "Hochpräzise technische Bauteile für industrielle Anwendungen. Druck- und stressgetestet.",
+        images: ["images/3d-1.jpg", "images/3d-2.jpg"]
+    },
+    {
+        id: "project-3d",
+        title: "Industrial 3D Parts",
+        category: "3d-print",
+        previewImage: "images/3d-preview.jpg",
+        description: "Hochpräzise technische Bauteile für industrielle Anwendungen. Druck- und stressgetestet.",
+        images: ["images/3d-1.jpg", "images/3d-2.jpg"]
     }
     // Du kannst hier beliebig viele Projekte hinzufügen
 ];
